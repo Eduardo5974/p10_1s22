47c581510dd8f361bd0cd7dffd80b188de1251d8
Eduardo5974 <144293055+Eduardo5974@users.noreply.github.com>
2023-09-13 18:57:48 -0600

    Add files via upload

---
+++ P10_1s22.java
@@ -0,0 +1,9 @@
+public class P10_1s22{
+	public static void main(String[] args) {
+	
+	  if (false){
+	    System.out.println("Por que aparezco...");
+	}
+	System.out.println("que paso...");
+  }
+}
